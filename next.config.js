ee88bd98c0a02ee360fed357fdd08d2dd6e190bc
codeinexx <jamesho5829@gmail.com>
2024-03-20 18:23:26 -0700

    updating nextconfigjs to allow pics from web to portfolio

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.pexels.com",
-            }
-        ]
-        }
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sabio.la",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
