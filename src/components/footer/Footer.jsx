0c65bc87559e81b7af3e28c7bb096d48dde4cbe5
James Ho <jamesho@J-Macbook-Pro.local>
2024-03-21 01:45:58 -0700

    Fix typo on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css"
 const Footer = () => {
   return (
   <div className={styles.container}> 
-  <div className={styles.logo}>Jamescodes0
+  <div className={styles.logo}>JamesCodes
     </div> 
     <div className={styles.text}>
   Copyright © 2023 James Ho | All Rights Reserved.
