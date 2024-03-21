9f33f6c60cf82cc6906e7d8bcbcf2f2d79fb78ed
codeinexx <jamesho5829@gmail.com>
2024-03-21 15:27:29 -0700

    Name change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,14 @@
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-  <div className={styles.container}> 
-  <div className={styles.logo}>JamesCodes
-    </div> 
-    <div className={styles.text}>
-  Copyright © 2023 James Ho | All Rights Reserved.
-  </div>
-  </div>
-  )
+    <div className={styles.container}>
+      <div className={styles.logo}>Codeinex</div>
+      <div className={styles.text}>
+        Copyright © 2023 James Ho | All Rights Reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
